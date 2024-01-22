adb5b7486e5158556b5766e4a3c53158c9a82d0f
Daniel Faller Grass <daniel-faller-grass@hotmail.com>
2024-01-22 18:41:41 -0300

    feat: add interceptors and using then

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -11,6 +11,7 @@ export class LogInterceptor implements NestInterceptor {
     return next
       .handle()
       .pipe(tap(() => console.log(request.url)))
+      .pipe(tap(() => console.log(request.method)))
       .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
     //O "tap" serve para executar uma ação antes ou depois da requisição pegando o resultado dela e fazendo algo com ele
   }
